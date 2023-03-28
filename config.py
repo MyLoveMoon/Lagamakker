d37f98943c4603039ad7035dce1580997468da33
ᴍʏʟᴏᴠᴇᴍᴏᴏɴ <90930470+MyLoveMoon@users.noreply.github.com>
2023-03-29 01:49:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,26 +5,26 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get it from my.telegram.org
-API_ID = ""
-API_HASH = ""
+API_ID = "26305268"
+API_HASH = "4e385c619185805f48427df36458d056"
 
 # Get it from @Botfather in Telegram.
-BOT_TOKEN = ""
+BOT_TOKEN = "5584170754:AAFVbY2ptBGKsQWUNBFbm7GfE0jrrG8NEyo"
 
 # Create Your Own ApiKey From @TechZApiBot To Use Logo Api
-API_KEY = ""
+API_KEY = "FFZYAL"
 
 
 # Don't Change Anything Below This Line
 
 
-if not API_ID or API_ID.strip() == "":
+if not API_ID or API_ID.strip() == "26305268":
     API_ID = int(getenv("API_ID"))
-if not API_HASH or API_HASH.strip() == "":
+if not API_HASH or API_HASH.strip() == "4e385c619185805f48427df36458d056":
     API_HASH = getenv("API_HASH")
-if not BOT_TOKEN or BOT_TOKEN.strip() == "":
+if not BOT_TOKEN or BOT_TOKEN.strip() == "5584170754:AAFVbY2ptBGKsQWUNBFbm7GfE0jrrG8NEyo":
     BOT_TOKEN = getenv("BOT_TOKEN")
-if not API_KEY or API_KEY.strip() == "":
+if not API_KEY or API_KEY.strip() == "FFZYAL":
     API_KEY = getenv("API_KEY")
 
 
